d96d2b86947441dfa2a4fa85a7fc2a54a89281f5
daniloagst <daniloaugusto.ita16@gmail.com>
2018-04-09 12:37:59 +0200

    fixing logging

--- dramatiq/logging.py
+++ dramatiq/logging.py
@@ -14,16 +14,43 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import absolute_import
 
+import sys
 import inspect
 import logging
 
+default_handler = logging.StreamHandler(sys.stdout)
+default_handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+))
 
-def get_logger(module, name=None):
+
+# @danilo here: shamelessly copied from Flask.
+def has_level_handler(logger):
+    level = logger.getEffectiveLevel()
+    current = logger
+    while current:
+        if any(handler.level <= level for handler in current.handlers):
+            return True
+        if not current.propagate:
+            break
+        current = current.parent
+    return False
+
+
+def get_logger(module, name=None, level=None):
     logger_fqn = module
     if name is not None:
         if inspect.isclass(name):
             name = name.__name__
         logger_fqn += "." + name
+    logger = logging.getLogger(logger_fqn)
+    # level
+    if logger.level == logging.NOTSET:
+        logger.setLevel(level or logging.DEBUG)
+    # handler
+    if not has_level_handler(logger):
+        logger.addHandler(default_handler)
 
-    return logging.getLogger(logger_fqn)
+    return logger
